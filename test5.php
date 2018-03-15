a0ddfad18da9a9705bb3e3ebed86c04e232c3186
Piyush Singh <piyushkumar194@gmail.com>
2018-03-15 21:43:37 +0530

    variable name print
    $var = 'PHP Tutorial'. Put this variable into the title section, h3 tag and as an anchor text within an HTML document

---
+++ test5.php
@@ -0,0 +1,15 @@
+<?php
+$var = 'PHP Tutorial';
+echo $var;
+
+ ?>
+ <html>
+<head>
+  <title><?php echo $var; ?> </title>
+</head>
+<body>
+  <h3><?php echo $var; ?> </h3> <br>
+  <a><?php echo $var; ?></a>
+
+</body>
+</html>
